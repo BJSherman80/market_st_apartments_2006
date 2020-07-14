bc5a2b55517b6d8d1da241bce163964dd3ef8a99
Brett Turing <bjsherman80@gmail.com>
2020-07-14 10:52:49 -0600

    Tried to fix add renters method

--- lib/building.rb
+++ lib/building.rb
@@ -1,3 +1,6 @@
+require './lib/renter'
+require './lib/apartment'
+
 class Building
   attr_reader :units, :renters
   def initialize
@@ -9,8 +12,12 @@ class Building
     @units << unit
   end
 
-  def add_renter(renter)
-    @renters << @unit[:renter]= (renter)
+  # def add_renter(renter)
+  #   @renters << @unit[:renter]= (renter)
+  # end
+  def renters
+    @renters << units.renter
   end
+
 
 end
